d8df407666be4501580a201ca98f40ab08e43eae
Gabriel Galang <galang69420@gmail.com>
2024-06-06 11:37:15 +0700

    revisi2

--- assets/menu/about_us.php
+++ assets/menu/about_us.php
@@ -6,7 +6,7 @@ include '../php/functions.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About UsL</title>
+    <title>About Us</title>
     <link rel="stylesheet" href="../bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/global.css">
     <link rel="stylesheet" href="../css/stye.css">
